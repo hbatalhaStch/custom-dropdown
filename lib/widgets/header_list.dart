15f9f09ed157f585eecd62352e2398667682f084
HBatalha <helderbatalha.sitech@gmail.com>
2024-03-11 17:29:11 -0100

    add a header list widget

---
+++ lib/widgets/header_list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HeaderList extends StatelessWidget {
+  final int itemCount;
+  final Widget Function(int index) label;
+  final void Function()? onDeleted;
+  const HeaderList({
+    super.key,
+    required this.itemCount,
+    required this.label,
+    this.onDeleted,
+  });
+
+  Widget getLabel(index) {
+    return label(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: ListView.separated(
+        itemCount: itemCount,
+        scrollDirection: Axis.horizontal,
+        separatorBuilder: (context, index) => const SizedBox(width: 5),
+        itemBuilder: (context, index) {
+          return Chip(
+            padding:
+                const EdgeInsets.only(left: 12, top: 0, right: 4, bottom: 0),
+            label: getLabel(index),
+            deleteIcon: const Icon(Icons.cancel_outlined, size: 18),
+            onDeleted: onDeleted,
+          );
+        },
+      ),
+    );
+  }
+}
